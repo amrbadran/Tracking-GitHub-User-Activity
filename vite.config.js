da19beb4ba3b6e91c59fbd0bfcb2db47e5cbed1d
unknown <badran.amr2003@gmail.com>
2024-12-29 00:29:04 +0200

    make deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "GitHub-User-Activity",
+  base: "/GitHub-User-Activity",
 });
